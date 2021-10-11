9a305e62523e58220d5657d6a73f575bb25aba8a
cristianoaffa <cristianoaffa@gmail.com>
2021-10-11 16:39:35 -0300

    starting development project

---
+++ src/main/java/com/freedom/datazipcodeconsumer/DataZipCodeConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.freedom.datazipcodeconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataZipCodeConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataZipCodeConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freedom/datazipcodeconsumer/DataZipCodeConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freedom.datazipcodeconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataZipCodeConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
